c4e49295580bcbd1bd0488f6686387b40ac4452b
egalbornoz <egalbornoz@gmail.com>
2026-03-05 23:30:38 -0400

    DB OK

--- database/config.js
+++ database/config.js
@@ -1,14 +1,15 @@
 import dotenv from 'dotenv';
-import mysql from 'mysql2/promise'; 
+import mysql from 'mysql2/promise';
 import colors from "colors";
 
 dotenv.config();
 
 const config = {
-    host: process.env.HOST, 
+    host: process.env.HOST,
     user: process.env.USER_SQL_DB,
     password: process.env.PASS_SQL_DB,
     database: process.env.SQL_DB,
+    port: 4000,
     waitForConnections: true,
     connectionLimit: 10, // Máximo 10 conexiones totales para TODA la app
     queueLimit: 0
@@ -33,12 +34,12 @@ export const db = async () => {
     try {
         // Ya no creamos el pool aquí, solo verificamos si está vivo
         const connection = await pool.getConnection();
-        connection.release(); 
-        
+        connection.release();
+
         console.log(colors.yellow.bold('DB MySQL: Pool compartido listo.'));
         return pool;
     } catch (error) {
         console.error(colors.red.bold('Error de conexión:', error));
-        throw error; 
+        throw error;
     }
 };
